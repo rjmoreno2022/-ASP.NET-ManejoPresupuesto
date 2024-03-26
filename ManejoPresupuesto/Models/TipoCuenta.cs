bbd09759c5d6a6d934ef77a02b424d040460edc4
Rafael Jimenez <125316019+rjmoreno2022@users.noreply.github.com>
2024-03-25 21:45:39 -0400

    108. Validaciones Personalizadas por Modelo

--- ManejoPresupuesto/Models/TipoCuenta.cs
+++ ManejoPresupuesto/Models/TipoCuenta.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace ManejoPresupuesto.Models
 {
-    public class TipoCuenta
+    public class TipoCuenta //: IValidatableObject
     {
         public int Id { get; set; }
         [Required(ErrorMessage = "El campo {0} es requerido")]
@@ -11,6 +11,18 @@ namespace ManejoPresupuesto.Models
         public string Nombre { get; set; }
         public int UsuarioId { get; set; }
         public int Orden { get; set; }
+
+        //public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        //{
+        //    if (Nombre != null && Nombre.Length > 0)
+        //    {
+        //        var primeraLetra = Nombre[0].ToString();
+        //        if (primeraLetra != primeraLetra.ToUpper())
+        //        {
+        //            yield return new ValidationResult("La primera letra debe ser mayúscula", new[] { nameof(Nombre) });
+        //        }
+        //    }
+        //}
 
     }
 }
